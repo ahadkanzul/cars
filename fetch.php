4deec9498914d353c8d5599425807d65e2f7e374
ahadkanzul <ahadkanzul@gmail.com>
2025-09-07 23:42:50 +0500

    fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($_GET['number'])) {
-    echo "No number provided.";
+    echo "<b>Error:</b> No number provided.";
     exit;
 }
 
@@ -21,6 +21,25 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 $response = curl_exec($ch);
 curl_close($ch);
 
-// Show raw response (you can parse it with regex/DOM if needed)
-echo $response;
+// If no response
+if (!$response) {
+    echo "<b>Error:</b> Unable to connect to Excise server.";
+    exit;
+}
+
+// Extract only the vehicle info table
+if (preg_match('/<table[^>]*>(.*?)<\/table>/is', $response, $matches)) {
+    $table = $matches[0];
+
+    // Clean table styles and keep plain text
+    $table = preg_replace('/style="[^"]*"/i', '', $table);
+    $table = preg_replace('/class="[^"]*"/i', '', $table);
+    $table = str_replace("td>", "td style='border:1px solid #00ff00; padding:5px;'>", $table);
+    $table = str_replace("th>", "th style='border:1px solid #00ff00; padding:5px;'>", $table);
+
+    echo "<h2 style='color:#00ff00;'>Vehicle Information</h2>";
+    echo $table;
+} else {
+    echo "<b>No record found</b> for number: <span style='color:#0f0;'>$number</span>";
+}
 ?>
